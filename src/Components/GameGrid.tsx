7487d36214582db1405b3e56393e95c3e3672942
suraj adhav <techsavvyhq.in@gmail.com>
2025-11-19 14:15:56 +0530

    Implementing Loading Skeleton for Game Card

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -3,13 +3,17 @@ import apiClient from "../services/api-client";
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1,2,3,4,5,6,7,,8,9,10,11,12];
+
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{sm:1, md:2, lg:3, xl:4}} spacing={5} padding={3}>
+      <SimpleGrid columns={{sm:1, md:2, lg:3, xl:4}} spacing={12} padding={3}>
+        {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton}/>)}
         {games.map((game) => {
           return <GameCard key={game.id} game={game}></GameCard>;
         })}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,19 +23,23 @@ interface FetchGameResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     apiClient
       .get<FetchGameResponse>("/games")
       .then((res) => {
         setGames(res.data.results);
+        setLoading(false);
       })
       .catch((err) => {
         setError(err.message);
+        setLoading(false);
       });
   }, []);
 
-  return {games, error};
+  return {games, error, isLoading};
 };
 
 export default useGames;
